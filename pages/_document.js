a9e90912ed1b87e69866f792e98547d1b7e09976
Minh Nguyen <minhnguyenit14@gmail.com>
2020-09-20 12:57:09 +0700

    keep trying... CORSSSS

--- pages/_document.js
+++ pages/_document.js
@@ -16,13 +16,13 @@ class MyDocument extends Document {
         <body>
           {/** ---- Facebook page-plugin required component --- */}
           <div id="fb-root"></div>
-          <script
+          {/* <script
             async
             defer
             crossorigin="anonymous"
             src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v8.0&appId=243311399938661&autoLogAppEvents=1"
             nonce="xhNa80zR"
-          ></script>
+          ></script> */}
           <script src="/fbMessenger.plugin.js"></script>
           {/** --- end region --- */}
           <Main />
